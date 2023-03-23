da0c591c030a619fc90f30279955e51144dc8ce5
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-03-23 11:45:21 -0300

    Adicionado funcoes: trataErro e pegaArquivo

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('olá mundo');
+import fs from 'fs';
+import chalk from 'chalk';
+import { text } from 'stream/consumers';
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.promises
+     .readFile(caminhoDoArquivo, encoding, )
+     .then((texto) => console.log(chalk.green(texto)))
+     .catch(trataErro)
+    }
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) =>  {
+//        if(erro) {
+//            trataErro(erro)
+//        }
+//        console.log(chalk.green(texto))
+//    }) 
+//}
+pegaArquivo('./arquivos/texto.md');
