451901cf7ada7a8d1246dc7ff3ac541d96089e9a
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-03-24 12:12:45 -0300

    Adicionado Função async

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 import fs from 'fs';
 import chalk from 'chalk';
-import { text } from 'stream/consumers';
 
 function trataErro(erro) {
+    console.log(erro)
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.promises
-     .readFile(caminhoDoArquivo, encoding, )
-     .then((texto) => console.log(chalk.green(texto)))
-     .catch(trataErro)
+// asyns | await
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile
+        (caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro);
     }
+}
 
-//function pegaArquivo(caminhoDoArquivo) {
-//    const encoding = 'utf-8';
-//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) =>  {
-//        if(erro) {
-//            trataErro(erro)
-//        }
-//        console.log(chalk.green(texto))
-//    }) 
-//}
+//promises com then()
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//      .readFile(caminhoDoArquivo, encoding, )
+//      .then((texto) => console.log(chalk.green(texto)))
+//      .catch(trataErro)
+// }
+
 pegaArquivo('./arquivos/texto.md');
