73f73dad7624c5dc8e4545db83c8d63d8dbb25ea
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-03-27 12:06:47 -0300

    Refatoração de código | Adicionado tratamento de erro na função processaTexto

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,36 @@
-import pegaArquivo from "./index.js";
+import chalk from 'chalk';
+import fs from 'fs';
+import pegaArquivo from './index.js';
 
 const caminho = process.argv;
 
-pegaArquivo(caminho[2]);
+function imprimeLista(resultado) {
+    console.log(chalk.yellow('lista de links'), resultado);
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    try{
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            console.log(chalk.red('Arquivo ou diretório não existe!'));
+            return;
+        }
+
+    }
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(argumentos[2]);
+        imprimeLista(resultado);
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(lista);
+        })
+    }
+}
+
+processaTexto(caminho);
