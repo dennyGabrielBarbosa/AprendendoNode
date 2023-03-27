b6863041d1b7abfba8d17fa755cf2b6a70ed7151
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-03-27 12:07:41 -0300

    Refatoração de código

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import chalk from 'chalk';
 function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)];
-    const resultado = capturas.map(captura => ({[captura[1]]: captura[2]}));
-    return resultado;
+    const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }));
+    return resultados.length !== 0 ? resultados : 'não há links no arquivo';
 }
 
 function trataErro(erro) {
@@ -17,9 +17,8 @@ function trataErro(erro) {
 async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
-        const texto = await fs.promises.readFile
-            (caminhoDoArquivo, encoding)
-        console.log(extraiLinks(texto))
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        return extraiLinks(texto);
     } catch (erro) {
         trataErro(erro)
     }
